96ab89d899ea507c3f1d064629f3334616792518
Fermin Minetto <fermin.minetto@gmail.com>
2020-10-20 17:40:50 -0300

    First Commit

---
+++ src/main/java/com/ferminmine/AmericaTransitAccidents/AmericaTransitAccidentsApplication.java
@@ -0,0 +1,13 @@
+package com.ferminmine.AmericaTransitAccidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmericaTransitAccidentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmericaTransitAccidentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferminmine/AmericaTransitAccidents/AmericaTransitAccidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferminmine.AmericaTransitAccidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmericaTransitAccidentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
